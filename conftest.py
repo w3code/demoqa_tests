568cd29e2bcc7042b05909cb1ff9d7af17eef4e7
Aleksei K <qatechnix@gmail.com>
2025-11-04 23:35:01 +0700

    add selenoid config

--- conftest.py
+++ conftest.py
@@ -1,12 +1,28 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.bidi.browser import Browser
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture
 def browser():
-    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    # browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+    options = Options()
+    selenoid_capabilities = {
+    "browserName": "chrome",
+    "browserVersion": "128.0",
+    "selenoid:options": {
+        "enableVideo": False
+    }
+}
+    options.capabilities.update(selenoid_capabilities)
+    browser = webdriver.Remote(
+        command_executor="http://localhost:4444/wd/hub",
+        options=options
+    )
     browser.set_window_size(1920, 1080)
     browser.implicitly_wait(5)
 
